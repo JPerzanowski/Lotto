b16252dd48df3cedf9869efed171004ef3b2e628
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-11-02 11:00:30 +0100

    Initial commit

---
+++ src/main/java/org/example/numberreceiver/NumberReceiverFacade.java
@@ -0,0 +1,12 @@
+package org.example.numberreceiver;
+
+import java.util.List;
+
+public class NumberReceiverFacade {
+    public String inputNumbers(List<Integer> numbers) {
+        int size = numbers.size();
+        if (size != 6)
+            return "Fail";
+        return "Success";
+    }
+}
---
+++ src/test/java/org/example/numberreceiver/NumberReceiverFacadeTest.java
@@ -0,0 +1,29 @@
+package org.example.numberreceiver;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class NumberReceiverFacadeTest {
+
+    @Test
+    void should_return_success_when_user_give_6_numbers() {
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        String result = numberReceiverFacade.inputNumbers(List.of(6, 7, 8, 40, 1, 78));
+        assert result.equals("Success");
+    }
+
+    @Test
+    void should_return_fal_when_user_give_more_than_6_numbers() {
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        String result = numberReceiverFacade.inputNumbers(List.of(6, 7, 8, 40, 1, 78, 65, 43));
+        assert result.equals("Fail");
+    }
+
+    @Test
+    void should_return_fal_when_user_give_less_than_6_numbers() {
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        String result = numberReceiverFacade.inputNumbers(List.of(6, 7, 8, 40));
+        assert result.equals("Fail");
+    }
+}

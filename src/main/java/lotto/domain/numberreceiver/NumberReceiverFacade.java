13e4b29a033836e10bc83b659c84e4e00bd96a27
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-11-05 14:25:43 +0100

    added clock, added repository and in memory testing

--- src/main/java/lotto/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/lotto/domain/numberreceiver/NumberReceiverFacade.java
@@ -1,12 +1,44 @@
 package lotto.domain.numberreceiver;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lotto.domain.numberreceiver.dto.InputNumberResultDto;
+import lotto.domain.numberreceiver.dto.TicketDto;
 
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@AllArgsConstructor
 public class NumberReceiverFacade {
-    public String inputNumbers(List<Integer> numbers) {
-        int size = numbers.size();
-        if (size != 6)
-            return "Fail";
-        return "Success";
+
+    private final NumberValidator validator;
+    private final NumberReceiverRepository repository;
+    private final Clock clock;
+
+    public InputNumberResultDto inputNumbers(Set<Integer> numbersFromUser) {
+        boolean areAllNumbersInRange = validator.areAllNumbersInRange(numbersFromUser);
+        if (areAllNumbersInRange) {
+            String ticketId = UUID.randomUUID().toString();
+            LocalDateTime drawDate = LocalDateTime.now(clock);
+            Ticket savedTicket = repository.save(new Ticket(ticketId, drawDate, numbersFromUser));
+            return InputNumberResultDto.builder()
+                    .drawDate(savedTicket.drawDate())
+                    .ticketId(savedTicket.ticketId())
+                    .numbersFromUser(numbersFromUser)
+                    .message("Success")
+                    .build();
+        }
+        return InputNumberResultDto.builder()
+                .message("Fail")
+                .build();
+    }
+
+    public List<TicketDto> userNumbers(LocalDateTime date) {
+        List<Ticket> allTicketsByDrawDate = repository.findAllTicketByDrawDate(date);
+        return allTicketsByDrawDate.stream()
+                .map(TicketMapper::mapFromTicket)
+                .toList();
     }
 }

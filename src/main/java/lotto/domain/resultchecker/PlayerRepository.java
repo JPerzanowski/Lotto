48f438e5e0111d688dc3d1f708cf226242719a64
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-11-08 11:39:22 +0100

    fixed problems with repo

--- src/main/java/lotto/domain/resultchecker/PlayerRepository.java
+++ src/main/java/lotto/domain/resultchecker/PlayerRepository.java
@@ -1,12 +1,10 @@
 package lotto.domain.resultchecker;
 
+
 import java.util.List;
 import java.util.Optional;
 
 public interface PlayerRepository {
-
-    List<Player> saveAll(List<Player> players);
-
     List<Player> saveAll(List<Player> players);
 
     Optional<Player> findById(String hash);

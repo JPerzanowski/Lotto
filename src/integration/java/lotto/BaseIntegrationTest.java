4907d18435c872d84c9bed5843ffa9158280081e
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-11-11 18:29:32 +0100

    saving and testing ticket

--- src/integration/java/lotto/BaseIntegrationTest.java
+++ src/integration/java/lotto/BaseIntegrationTest.java
@@ -1,7 +1,6 @@
 package lotto;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
-import lotto.domain.LottoSpringBootApplication;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
---
+++ src/main/java/lotto/infrastructure/AasdasdRepository.java
@@ -0,0 +1,8 @@
+package lotto.infrastructure;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AasdasdRepository extends MongoRepository<Ticket, String> {
+}

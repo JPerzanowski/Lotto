24869c2bf796ded8f60a1d5d3297b81294bbc61b
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-11-14 23:23:27 +0100

    added spring boot data mongodb

--- src/main/java/lotto/LottoSpringBootApplication.java
+++ src/main/java/lotto/LottoSpringBootApplication.java
@@ -4,11 +4,13 @@ import lotto.domain.numbergenerator.WinningNumbersGeneratorFacadeConfigurationPr
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties({WinningNumbersGeneratorFacadeConfigurationProperties.class})
+@EnableMongoRepositories
 public class LottoSpringBootApplication {
 
     public static void main(String[] args) {
--- src/main/java/lotto/domain/numbergenerator/WinningNumbers.java
+++ src/main/java/lotto/domain/numbergenerator/WinningNumbers.java
@@ -1,11 +1,14 @@
 package lotto.domain.numbergenerator;
 
 import lombok.Builder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 @Builder
-record WinningNumbers(
+@Document
+record WinningNumbers(@Id
                       String id,
                       Set<Integer> winningNumbers,
                       LocalDateTime date) {

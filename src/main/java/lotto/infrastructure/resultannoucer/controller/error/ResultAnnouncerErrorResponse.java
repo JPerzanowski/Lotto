fa59fc883eaf7bc573c4a03edcb899a685b5cbe5
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-11-18 23:16:02 +0100

    added exception handler

---
+++ src/main/java/lotto/infrastructure/resultannoucer/controller/error/ResultAnnouncerErrorResponse.java
@@ -0,0 +1,8 @@
+package lotto.infrastructure.resultannoucer.controller.error;
+
+import org.springframework.http.HttpStatus;
+
+public record ResultAnnouncerErrorResponse(
+        String message,
+        HttpStatus status) {
+}

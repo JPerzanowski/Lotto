685ce1e91a1e43d3fea304be18c4307e62306bcb
Jakub Perzanowski <jakub_perzanowski@o2.pl>
2023-11-21 11:12:20 +0100

    added left steps in scenario result checker and result announcer

--- src/main/java/lotto/domain/numberreceiver/TicketRepository.java
+++ src/main/java/lotto/domain/numberreceiver/TicketRepository.java
@@ -1,13 +1,12 @@
 package lotto.domain.numberreceiver;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.time.LocalDateTime;
 import java.util.Collection;
-
-public interface TicketRepository {
+public interface TicketRepository extends MongoRepository<Ticket, String> {
 
     Collection<Ticket> findAllTicketsByDrawDate(LocalDateTime drawDate);
 
     Ticket findByHash(String hash);
-
-    Ticket save(Ticket savedTicket);
 }
--- src/main/java/lotto/domain/resultannoucer/ResultAnnouncerFacade.java
+++ src/main/java/lotto/domain/resultannoucer/ResultAnnouncerFacade.java
@@ -8,7 +8,6 @@ import lotto.domain.resultchecker.dto.ResultDto;
 
 import java.time.Clock;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Optional;
 
 import static lotto.domain.resultannoucer.MessageResponse.*;
@@ -16,7 +15,6 @@ import static lotto.domain.resultannoucer.MessageResponse.*;
 @AllArgsConstructor
 public class ResultAnnouncerFacade {
 
-    public static final LocalTime RESULTS_ANNOUNCEMENT_TIME = LocalTime.of(12, 0).plusMinutes(5);
     private final ResultCheckerFacade resultCheckerFacade;
     private final ResponseRepository responseRepository;
     private final Clock clock;
@@ -28,7 +26,7 @@ public class ResultAnnouncerFacade {
                 return new ResultAnnouncerResponseDto(ResultMapper.mapToDto(resultResponseCached.get()), ALREADY_CHECKED.info);
             }
         }
-        ResultDto resultDto = resultCheckerFacade.findByHash(hash);
+        ResultDto resultDto = resultCheckerFacade.findByTicketId(hash);
         if (resultDto == null) {
             return new ResultAnnouncerResponseDto(null, HASH_DOES_NOT_EXIST_MESSAGE.info);
         }
@@ -37,7 +35,7 @@ public class ResultAnnouncerFacade {
         if (responseRepository.existsById(hash) && !isAfterResultAnnouncementTime(resultDto)) {
             return new ResultAnnouncerResponseDto(responseDto, WAIT_MESSAGE.info);
         }
-        if (resultCheckerFacade.findByHash(hash).isWinner()) {
+        if (resultCheckerFacade.findByTicketId(hash).isWinner()) {
             return new ResultAnnouncerResponseDto(responseDto, WIN_MESSAGE.info);
         }
         return new ResultAnnouncerResponseDto(responseDto, LOSE_MESSAGE.info);
@@ -64,7 +62,7 @@ public class ResultAnnouncerFacade {
     }
 
     private boolean isAfterResultAnnouncementTime(ResultDto resultDto) {
-        LocalDateTime announcementDateTime = LocalDateTime.of(resultDto.drawDate().toLocalDate(), RESULTS_ANNOUNCEMENT_TIME); //
+        LocalDateTime announcementDateTime = resultDto.drawDate();
         return LocalDateTime.now(clock).isAfter(announcementDateTime);
     }
 
